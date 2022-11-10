67875c027f83daaec160349393b29a6f49167358
BarbaraGB1 <bargarbo@gmail.com>
2022-11-10 18:19:54 +0100

    button twitter

--- src/js/6_local.js
+++ src/js/6_local.js
@@ -12,6 +12,7 @@
 let cardUrl;
 
 function localCard(){
+ 
     fetch('https://awesome-profile-cards.herokuapp.com/card', {
   method: 'POST', // Para enviar datos
   body: JSON.stringify(userData),
@@ -23,10 +24,12 @@ function localCard(){
   .then(responseJson =>{
     if (responseJson.success) {
         cardUrl = responseJson.cardURL;
+        cardCreated.classList.remove('hidden');
         textError.innerHTML = 'La tarjeta ha sido creada:';
         twitter.classList.remove('hidden');
         linkshare.innerHTML = cardUrl; 
         linkshare.href = cardUrl;
+        twitter.href = `https://twitter.com/intent/tweet?text=Nueva%20tarjeta%20creada%20${cardUrl}`;
     }else{
         cardCreated.style = 'border-top: solid 1px $border-color;';
         linkshare.style = 'display: block;';
@@ -34,3 +37,4 @@ function localCard(){
     }
   });
 }
+
